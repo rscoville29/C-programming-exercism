573aa4edb0b082c20a843f7b3e5a96d3806f216e
Ryan <85287770+rscoville29@users.noreply.github.com>
2026-03-29 05:19:57 -0600

    updated logic. Passing 5/10 tests

--- resistor_color_trio/resisitor_color_trio.c
+++ resistor_color_trio/resisitor_color_trio.c
@@ -9,13 +9,18 @@ resistor_value_t color_code(resistor_band_t bands[]){
         resistor_value_t result;
         int base = getCode(bands[0]) * 10 + getCode(bands[1]);
         int multiplier = getCode(bands[2]);
-    if(multiplier > 0){
-        result.value = base + pow(10, multiplier);
+    if(multiplier < 3){
+        result.value = base * pow(10, multiplier);
         result.unit = OHMS;
-    }else{
+    }else if(multiplier == 3){
         result.value = base;
-        result.unit = OHMS;
+        result.unit = KILOOHMS;
+    }else if(multiplier == 4){
+        result.value = base;
+        result.unit = MEGAOHMS;
+    }else if(multiplier == 5){
+        result.value = base;
+        result.unit = GIGAOHMS;
     }
-        
         return result;
 }
